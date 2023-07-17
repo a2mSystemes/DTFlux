681481a2902a7c088ea19d72c990e17ff911f669
Cornehu <jonathan63530@gmail.com>
2023-07-17 13:26:20 +0200

    add iRunner
    Co-authored-by: Ange-Marie MAURIN <contact@a2msystemes.fr>

---
+++ dtflux-player/src/app/displays/stream-full/stream-full.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { MockingService } from 'src/app/services/mocking.service';
+
+@Component({
+  selector: 'app-stream-full',
+  templateUrl: './stream-full.component.html',
+  styleUrls: ['./stream-full.component.sass']
+})
+
+export class StreamFullComponent implements OnInit{
+  data?:any;
+  sub: Subscription;
+  constructor(private mockingService:MockingService){
+    this.data = null;
+
+    this.sub = this.mockingService.data$.subscribe({
+      "next": (data) => {
+        console.log(data);
+        this.data = data;
+        //data.photo1 et photo2 et photo3 à alimenter aussi
+        data.ContestId = 1;
+        data.display = "podium"; // valeur = "podium" pour afficher l'écran du podium OU
+        //data.display = "recap" //valeur = "recap" pour afficher le tableau des athlètes
+        data.bib1 = "109";
+        data.lastname1 = "Seraphina";
+        data.firstname1 = "Raphael";
+        data.lastnameR1 = "Bachibouzouk";
+        data.firstnameR1 = "ignesta";
+        data.totaltime1 = "1:26:44";
+        data.bib2 = "217";
+        data.lastname2 = "Jonas";
+        data.firstname2 = "Michel";
+        data.lastnameR2 = "Mannotto";
+        data.firstnameR2 = "Francis";
+        data.totaltime2 = "1:28:17";
+        data.bib3 = "30";
+        data.lastname3 = "Maurin";
+        data.firstname3 = "Ange-Marie";
+        data.lastnameR3 = "Chassaing";
+        data.firstnameR3 = "Jonathan";
+        data.totaltime3 = "1:28:35";
+      },
+      "error": (err) => {console.log(err);},
+    });
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
